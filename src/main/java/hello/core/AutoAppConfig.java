1600d066eb92103b4fc899b771974773f5c24785
rlatldhs32 <rlatldhs32@naver.com>
2022-03-09 20:02:16 +0900

    basePackage 설정 220309 Update

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration //설정정보
 @ComponentScan(
+        basePackages = "hello.core.member",//베이스 지정
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes=Configuration.class)
         //컴포넌트 스캔으로 다 뒤져서 스프링 빈으로 등록하는데,뺄꺼를 지정해줌
 )
